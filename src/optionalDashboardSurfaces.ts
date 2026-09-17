3640a59a07282aef01730aeae203d8497b7f2b38
Cristian Tcaci <59696583+Chris0Jeky@users.noreply.github.com>
2026-09-17 17:09:16 +0100

    test: remove unused loader probe

--- src/optionalDashboardSurfaces.ts
+++
@@ -1,9 +0,0 @@
-export async function loadShareStudio() {
-  const module = await import('./components/ShareStudio')
-  return { default: module.ShareStudio }
-}
-
-export async function loadWrappedExperience() {
-  const module = await import('./components/WrappedExperience')
-  return { default: module.WrappedExperience }
-}
